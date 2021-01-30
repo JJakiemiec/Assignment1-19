cdf51bf5955d3d7ba2b75d547d054627d75b6f3a
Rose Peters <rmpeters3@alaska.edu>
2021-01-29 16:41:24 -0900

    Updated test cases

--- Project3/Project3/StringCalculator.cpp
+++ Project3/Project3/StringCalculator.cpp
@@ -1,9 +1,24 @@
 #include "StringCalculator.hpp"
 #include <string>
 using std::string;
+#include <iostream>
 
 int stringCalculator(string equation) {
 	if (equation == "")
 		return 0;
+		if (equation.length()==1) {
 	return equation[0] - '0';
+		}
+int index;
+index = equation.find(",");
+
+string word;
+string word2;
+
+word = equation.substr(0,index);
+word2 = equation.substr(index);
+
+std::cout << word << std::endl << word2;
+
+return 0;
 }
